07c6809fa726e371dc38a53d35e7e65d27d90309
amelzourane <amelzourane@monboreal.ca>
2025-10-15 14:41:58 -0400

    Exercice 4 :Types et media

---
+++ exo4.dart
@@ -0,0 +1,37 @@
+class Media {
+  final String titre;
+  Media(this.titre);
+
+  void afficherType() {
+    print("Ceci est un média générique.");
+  }
+}
+
+class Livre extends Media {
+  final String auteur;
+  Livre(String titre, this.auteur) : super(titre);
+
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+
+class Film extends Media {
+  final int dureeMinutes;
+  Film(String titre, this.dureeMinutes) : super(titre);
+
+  void afficherType() {
+    print("Ceci est un Film : $titre (${dureeMinutes}min).");
+  }
+}
+
+void main() {
+  List<Media> catalogue = [
+    Livre("L'Étranger", "Albert Camus"),
+    Film("Inception", 148)
+  ];
+ for (var m in catalogue) {
+    m.afficherType();
+}
+}
+
